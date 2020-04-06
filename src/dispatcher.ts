04233abe864a841f4d3175b624ef3cc10d12ad78
Michael Brich <mike@havenlight.net>
2020-04-05 23:15:44 -0700

    Cleanup

--- src/dispatcher.ts
+++ src/dispatcher.ts
@@ -5,6 +5,7 @@ import {EventEmitter} from 'events';
 export class ArmorLogDispatcher {
 	public readonly events: EventEmitter;
 	public readonly handlers: {[level: string]: ArmorLogHandler[]};
+	public readonly logGroups: string[];
 
 	constructor(events: EventEmitter) {
 		if (!events) {
@@ -15,19 +16,34 @@ export class ArmorLogDispatcher {
 			throw new Error('Armor Log Dispatcher init failed - events argument was not a valid EventEmitter instance.');
 		}
 
+		this.logGroups = [];
 		this.handlers = {};
 		this.events = events;
 	}
 
-	public createHandlerGroups(): any {
 
+	public register(level: ArmorLogLevel, handler: ArmorLogHandler): string {
+		const levelStr = level.toString();
+		if (!this.handlers[levelStr]) {
+			this.handlers[levelStr] = [];
+		}
+
+		this.handlers[levelStr].push(handler);
+		return '';
 	}
 
-	public register(level: ArmorLogLevel): void {
-
+	public unregister(id: string): boolean {
+		return false;
 	}
 
 	public dispatch(level: ArmorLogLevel): void {
+
+		for (let i = 0; i < this.logGroups.length; i++) {
+
+		}
+	}
+
+	public createHandlerId(): void {
 
 	}
 }
