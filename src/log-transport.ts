5818aa5122ab4ac6bed799e298fc435c5166cee1
SaxanLosterToreda <70411010+SaxanLosterToreda@users.noreply.github.com>
2020-10-05 12:46:15 -0700

    change bitwise lint from error to warning in this project.

--- src/log-transport.ts
+++ src/log-transport.ts
@@ -34,7 +34,7 @@ export class LogTransport {
 		return execute;
 	}
 
-	public defaultAction: LogTransportAction = function (logMessage) {
+	public defaultAction: LogTransportAction = (logMessage) => {
 		return new Promise((resolve) => {
 			let logString = '';
 			logString += `[${logMessage.date}]`;
